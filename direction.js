74eb86a167c787bc802d0c1db3982213ea7f6a25
evabrandenburg <evabrandenburg89@gmail.com>
2014-06-10 18:57:59 -0400

    simpler js

--- direction.js
+++ direction.js
@@ -1,91 +1,39 @@
-function output( number ) {
+/*function output( number ) {
 
-};
+};*/
 
 //http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?home_latitude=5.5&home_longitude=0&work_latitude=40.714&work_longitude=-73.989
 
  $.getJSON( "http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?"
-    		+"home_latitude="
-    		+latitude
-			+"&home_longitude="
-			+longitude
-    		+"&work_latitude=40.714"
-    		+"&work_longitude=-73.989",
-            $.each(results[0].address_components, function(){
-             $("#start").find('input[name="'+ this.types+'"]').attr('value', this.long_name);
-            };
+		+"home_latitude="
+		+latitude
+		+"&home_longitude="
+		+longitude
+		+"&work_latitude=40.714"
+		+"&work_longitude=-73.989",
+        $.each(results[0].address_components, function(){
+         $("#start").find('input[name="'+ this.types+'"]').attr('value', this.long_name);
+        };);
 
-    		 function ( data ) {
-    		 	var start0 = $( '#start0' );
-    		 	var start1 = $( '#start1' );
-    		 	var start2 = $( '#start2' );
-    		 	var finish0 = $( '#finish0' );
-    		 	var finish1 = $( '#finish1' );
-    		 	var finish2 = $( '#finish2' );
-    		 	var tooFar = data.begin_at[0].blocks
-    		 	var warning = $( '.warning');
-    		 	start0.text(
-    		 		data.begin_at[0].slots
-    		 		+ " bikes at "
-    		 		+ data.begin_at[0].name  
-    		 		+ ", " 
-    		 		+ data.begin_at[0].blocks  
-    		 		+ " blocks away"
-    		 	);
-    		 	start1.text(
-    		 		data.begin_at[1].slots
-    		 		+ " bikes at "
-    		 		+ data.begin_at[1].name 
-    		 		+ ", " 
-    		 		+ data.begin_at[1].blocks 
-    		 		+ " blocks away"
-    		 	);
-    		 	start2.text(
-    		 		data.begin_at[2].slots
-    		 		+ " bikes at "
-    		 		+ data.begin_at[2].name
-    		 		+ ", " 
-    		 		+ data.begin_at[2].blocks 
-    		 		+ " blocks away"
-    		 	);
-    		 	finish0.text(
-    		 		data.finish_at[0].slots
-    		 		+ " docks at "
-    		 		+ data.finish_at[0].name 
-    		 		+ ", " 
-    		 		+ data.finish_at[0].blocks 
-    		 		+ " blocks away"
-    		 	);
-    		 	finish1.text(
-    		 		data.finish_at[1].slots
-    		 		+ " docks at "
-    		 		+ data.finish_at[1].name
-    		 		+ ", " 
-    		 		+ data.finish_at[1].blocks
-    		 		+ " blocks away"
-    		 	);
-    		 	finish2.text(
-    		 		data.finish_at[2].slots
-    		 		+ " docks at "
-    		 		+(data.finish_at[2].name) 
-    		 		+ ( ", " )
-    		 		+ ( data.finish_at[2].blocks ) 
-    		 		+ " blocks away"
-    		 	);
-    		 	if(tooFar > 20) {
-    		 		warning.text("That's pretty far bro, maybe you shouldn't bike");
-    		 	}
+		 function ( data ) {
+		 	var start0 = $( '#start0' );
+		 	
+		 	start0.text(
+		 		data.begin_at[0].slots
+		 		+ " bikes at "
+		 		+ data.begin_at[0].name  
+		 		+ ", " 
+		 		+ data.begin_at[0].blocks  
+		 		+ " blocks away"
+		 	);
+		 };
+    ); 
 
-    		 }
-    	); 
-	);
-}
 
 
 
 var latitude;
-
-console.log( latitude );
+var longitude;
 
 var button = $( "button" );
 var where = $( "#where" ); 
